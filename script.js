3352c07b8ce948eca718b1a7438db0bc405dec4c
ysengrimm <ysengrimm@grimmbart.org>
2019-01-25 20:13:38 +0100

    Add question area and buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const leftButton = document.querySelector("#left");
+const rightButton = document.querySelector("#right");
+
+leftButton.addEventListener("click", () => {
+  console.log("left");
+})
+
+rightButton.addEventListener("click", () => {
+  console.log("right");
+})
