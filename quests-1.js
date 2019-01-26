a9104681989093cc88204743068f45bde862dc6c
Till Isenhuth <till+dwellr@isenhuth.de>
2019-01-26 19:23:09 +0100

    new Quests (german)

---
+++ quests-1.js
@@ -0,0 +1,44 @@
+const quests = [
+  {
+    "text": "How are you today?",
+    "npc": "John Snow",
+    "choices": [
+      {
+        "text": "Good!",
+        "impact": {
+          "nature": 0.2
+        }
+      },
+      {
+        "text": "Bad!",
+        "impact": {
+          "public": -0.3
+        }
+      }
+    ]
+  },
+  {
+    "text": "What time is it?",
+    "npc": "John Snow",
+    "choices": [
+      {
+        "text": "I dont know!",
+        "impact": {
+          "nature": 0.1
+        }
+      }
+    ]
+  },
+  {
+    "text": "What time is it?",
+    "npc": "Blah Blubb",
+    "choices": [
+      {
+        "text": "I dont know!",
+        "impact": {
+          "public": 0.1
+        }
+      }
+    ]
+  }
+];
