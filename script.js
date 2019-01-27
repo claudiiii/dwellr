413f352ef63cb6a14aabbbfdfd194db7c372c4c6
Till Isenhuth <till+dwellr@isenhuth.de>
2019-01-27 15:56:38 +0100

    svg v1

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 class Game {
 
   constructor() {
     this.leftButton = document.querySelector("#left");
     this.rightButton = document.querySelector("#right");
 
-    this.ownerScoreText = document.querySelector("#owner").querySelector(".score");
+    this.ownerScoreText = document.querySelector("#ownerScore").querySelector(".score");
     this.natureScoreText = document.querySelector("#nature").querySelector(".score");
     this.userScoreText = document.querySelector("#user").querySelector(".score");
     this.publicScoreText = document.querySelector("#public").querySelector(".score");
@@ -122,7 +121,7 @@ class Game {
     this.leftActionText.textContent = leftChoice["text"];
     this.rightActionText.textContent = rightChoice === undefined ? leftChoice["text"] : rightChoice["text"];
 
-    this.ownerScoreText.textContent = this.ownerScore;
+    this.ownerScoreText.setAttribute("offset", this.ownerScore);
     this.natureScoreText.textContent = this.natureScore;
     this.userScoreText.textContent = this.userScore;
     this.publicScoreText.textContent = this.publicScore;
